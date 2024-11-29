d5bc705b1fe18989974f8e28ac493bb7ffe599eb
NastassjaRosa <nastassja.paulhiac@gmail.com>
2024-11-29 09:56:40 +0100

    Corrections et JavaDoc

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,54 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+
 import java.io.FileReader;
 import java.io.FileWriter;
-// Initial commit 'test' dev branch
+
+
+/**
+ * The type Analytics counter.
+ */
 public class AnalyticsCounter {
 	private static int headacheCount = 0;	// initialize to 0
 	private static int rashCount = 0;		// initialize to 0
 	private static int pupilCount = 0;		// initialize to 0
-	//hey ho
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws Exception the exception
+     */
+    public static void main(String [] args) throws Exception {
+        System.out.println("Répertoire courant : " + System.getProperty("user.dir"));
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        // first get input
+        int headacheCount;
+        try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"))) {
+            String line = reader.readLine();
+
+            headacheCount = 0;
+            while (line != null) {
+                System.out.println("symptom from file: " + line);
+                if (line.equals("headache")) {
+                    headacheCount++;
+                    System.out.println("number of headaches: " + headacheCount);
+                } else if (line.equals("rash")) {
+                    rashCount++;
+                } else if (line.contains("pupils")) {
+                    pupilCount++;
+                }
+
+                line = reader.readLine();    // get another symptom
+            }
+        }
+
+        // next generate output
+        try (FileWriter writer = new FileWriter("result.out")) {
+            writer.write("headache: " + headacheCount + "\n");
+            writer.write("rash: " + rashCount + "\n");
+            writer.write("dilated pupils: " + pupilCount + "\n");
+        }
+    }
 }
